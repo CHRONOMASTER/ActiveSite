5fb8e1abb80d6df1bc46c4d13efabeaab770797c
Tristan Burns <silver.sky.hacker@gmail.com>
2018-09-11 13:08:22 -0700

    Active Site work from Summer 2018

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObject : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public enum MouseButtom { Primary, Secondary, Middle };
+
+    public MouseButtom mouseButtom = MouseButtom.Primary;
+
+    RectTransform rect;
+    Vector3 startClickPos;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startClickPos = Input.mousePosition;
+        Debug.Log(startClickPos);
+    }
+
+    public void OnPointerDrag(PointerEventData eventData)
+    {
+        if (Input.GetMouseButton((int)mouseButtom))
+        {
+            rect.localPosition = Input.mousePosition - startClickPos;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        rect.localPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
